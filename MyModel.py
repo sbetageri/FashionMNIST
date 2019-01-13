3ec86d21c7f820518057e1d1cbb032b98e7d93dd
Srirakshith Betageri <sbetageri111@gmail.com>
2019-01-12 20:23:40 -0600

    Minor changes to model. 91% accuracy

--- MyModel.py
+++ MyModel.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 class MyModel(nn.Module):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)
-        
-        self.fc1 = nn.Linear(in_features=64 * 4 * 4, out_features=128)
-        self.fc2 = nn.Linear(in_features=128, out_features=10)
+        self.checkingFlag = 0
 
-        self.dropout = nn.Dropout(p=0.4)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=3)
+        
+        self.fc1 = nn.Linear(in_features=50 * 5 * 5, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=10)
+
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2)
         self.logsoftmax = nn.LogSoftmax(dim=1)
@@ -26,21 +27,21 @@ class MyModel(nn.Module):
         x = self.conv1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-        x = self.dropout(x)
         
         x = self.conv2(x)
         x = self.relu(x)
         x = self.maxpool(x)
-        x = self.dropout(x)
+
+        if self.checkingFlag == 1:
+            print(x.size())
+            assert False
 
         x = x.view(x.size()[0], -1)
         
         x = self.fc1(x)
         x = self.relu(x)
-        x = self.dropout(x)
         
         x = self.fc2(x)
-        x = self.relu(x)
         x = self.logsoftmax(x,)
 
         return x
